20c8c8bc765e0492c3d6942f5e37f4d7493decd4
unknown <rmaurer99@gmail.com>
2023-10-26 03:07:35 -0500

    Finished code for password generator criterria

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,49 @@
 // Assignment code here
+function generatePassword() {
+  // Prompt user for password criteria
+  var length = prompt("Please enter the length of your password. Password length must be between 8 and 128 characters.");
+  if (length < 8 || length > 128) {
+    alert("Password length must be between 8 and 128 characters.");
+    return "";
+  }
+  var includeLowercase = confirm("Click OK to confirm including lowercase characters.");
+  var includeUppercase = confirm("Click OK to confirm including uppercase characters.");
+  var includeNumbers = confirm("Click OK to confirm including numeric characters.");
+  var includeSpecial = confirm("Click OK to confirm including special characters.");
 
+  // Define character sets based on user criteria
+  var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+  var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numericChars = "0123456789";
+  var specialChars = "!@#$%^&*()-_=+,<.>/?";
+
+  // Initialize password variable
+  var password = "";
+
+  // Build character set based on user criteria
+  var charSet = "";
+  if (includeLowercase) {
+    charSet += lowercaseChars;
+  }
+  if (includeUppercase) {
+    charSet += uppercaseChars;
+  }
+  if (includeNumbers) {
+    charSet += numericChars;
+  }
+  if (includeSpecial) {
+    charSet += specialChars;
+  }
+
+  // Generate password
+  for (var i = 0; i < length; i++) {
+    var randomIndex = Math.floor(Math.random() * charSet.length);
+    password += charSet[randomIndex];
+  }
+
+  // Return generated password
+  return password;
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Password Generator Starter Code
+This application creates a password for a user based on what criteria the user wants in their password.<br>
+### Password criteria that the user can choose from include:
+Length, 8-128 characters,<br>
+Lowercase,<br>
+Uppercase,<br>
+Numbers,<br>
+Special Characters<br><br>
+When the user has completed selecting criteria, a password is gernerated.
